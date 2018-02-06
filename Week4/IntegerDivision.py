220cd70ad73fb9f99cfddf8f989274c312af0bd8
Aakash <kumar.aakash10@gmail.com>
2018-02-06 15:32:19 +0530

    Update IntegerDivision.py

--- Week4/IntegerDivision.py
+++ Week4/IntegerDivision.py
@@ -5,7 +5,7 @@ def integerDivision(x, a):
 
     returns: integer, the integer division of x divided by a.
     """
-    count = 0 
+    count = 0                         """ New Line Added to remove the error """
     while x >= a:
         count += 1
         x = x - a
